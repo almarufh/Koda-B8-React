73d505b1e4f0a04c5812089d273d36afc1296173
almarufh <hidayatmaruf99@gmail.com>
2026-06-13 08:43:02 +0800

    fix: used parameter function props espected output is object for simple write

--- src/components/auth/InputField.jsx
+++ src/components/auth/InputField.jsx
@@ -1,23 +1,23 @@
-function InputField({ label, name, type, placeholder, value, onChange, src }) {
+function InputField(props) {
   return (
     <div className="flex flex-col gap-1">
-        <label htmlFor={name} className="text-[14px] text-[#111827]">
-            {label}
+        <label htmlFor={props.name} className="text-[14px] text-[#111827]">
+            {props.label}
         </label>
         <div 
             className="flex items-center gap-3 border border-[#0000001A] bg-[#F3F4F6] py-[13.5px] px-[16px] rounded-[12px]">
             <img 
-                src={src} 
-                alt={name} 
+                src={props.src} 
+                alt={props.name} 
             />
             <input
-                id={name}
+                id={props.name}
                 className="bg-transparent w-full outline-none"
-                name={name}
-                type={type}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
+                name={props.name}
+                type={props.type}
+                placeholder={props.placeholder}
+                value={props.value}
+                onChange={props.onChange}
                 required
             />
         </div>
